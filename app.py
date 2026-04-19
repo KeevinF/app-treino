5d282ae2d50c2b665e73df1630fb6304f716038a
flpmths <fellipee_m@hotmail.com>
2026-04-19 00:37:08 -0300

    Adicionado caminhos login/index

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
-from flask_livereload import LiveReload
 
 app = Flask (__name__)
 app.secret_key = 'dev'
-LiveReload(app)
 
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/logar', methods=['POST'])
+def logar():
+    return redirect(url_for('index'))
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
